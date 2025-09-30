349967fcaa5bafbe72ac37b3e2ae8fe84ad82a71
nemasha-demel <nemashahirushidemel@gmail.com>
2025-09-30 16:26:58 +0530

    Update API base URL to use environment variable

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const Api = createApi({      
   reducerPath: "Api",      
   baseQuery: fetchBaseQuery({        
-    baseUrl: "http://localhost:8000/api",        
+    baseUrl: `${import.meta.env.VITE_BASE_URL}/api`,     
     prepareHeaders: async (headers) => {          
       return new Promise((resolve) => {            
         async function checkToken() {              
